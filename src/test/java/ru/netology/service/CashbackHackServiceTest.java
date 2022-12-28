7ec34bb4d2644a36d68ea4704e9e0dcc13f9ff6e
Andrey Petrov <Pandand1@yandex.ru>
2022-12-28 14:39:40 +0300

    Add branch junit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,28 +1,56 @@
 package ru.netology.service;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
-class CashbackHackServiceTest {
+public class CashbackHackServiceTest {
 
     @Test
     public void remain() {
+        int amount = 900;
+        int expected = 100;
+
         CashbackHackService service = new CashbackHackService();
 
-        int expected = 100;
-        int actual = service.remain(900);
+        int actual = service.remain(amount);
 
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
     }
 
     @Test
-    public void ifPurchaseEquals1000() {
+    public void ifBuyOn1() {
+        int amount = 1;
+        int expected = 999;
+
         CashbackHackService service = new CashbackHackService();
 
-        int expected = 0;
-        int actual = service.remain(1000);
+        int actual = service.remain(amount);
 
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void ifBuyOn999() {
+        int amount = 999;
+        int expected = 1;
+
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void ifBuyOn1000() {
+        int amount = 1000;
+        int expected = 0;
+
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
     }
 }
